addef74f5717f79d9b15db849a60381105c902b8
Kyle Johnston <klj70143@uga.edu>
2020-06-19 20:35:46 -0400

    more homepage work

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,131 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './styles.scss';
+import "./styles.scss";
+import owl from "./../../assets/owl.jpg";
 
 class Home extends Component {
-    render () {
-        return (
-            <div className="main">
-                <div className="wrap">
-                    <div className="container">
-                        words
-                    </div>
-                </div>
-                <div>
-                    test
-                </div>
+  render() {
+    return (
+      <div className="homepage">
+        <div className="wrap">
+          <div className="container">
+            <h1>Think Local</h1>
+            <div className="desc">
+              Shop the local and popular brands you love
             </div>
-        );
-    }
-};
+            <div className="wrapper">
+              <a href="/products">FIND TREASURES</a>
+            </div>
+          </div>
+        </div>
+        <div className="featured">
+          <div className="title">Featured Products</div>
+          <div className="products">
+            <div className="productRow">
+              <a href="/products/item" alt="item">
+                <div className="item">
+                  <div className="container">
+                    <img src={owl} alt="itemImg"></img>
+                    <div className="overlay">
+                      <div className="text">View Item</div>
+                    </div>
+                  </div>
+                  <div className="productName">Nora Flemming Mini - Owl</div>
+                  <div className="price">$99.99</div>
+                </div>
+              </a>
+              <a href="/products/item" alt="item">
+                <div className="item">
+                  <div className="container">
+                    <img src={owl} alt="itemImg"></img>
+                    <div className="overlay">
+                      <div className="text">View Item</div>
+                    </div>
+                  </div>
+                  <div className="productName">Nora Flemming Mini - Owl</div>
+                  <div className="price">$99.99</div>
+                </div>
+              </a>
+              <a href="/products/item" alt="item">
+                <div className="item">
+                  <div className="container">
+                    <img src={owl} alt="itemImg"></img>
+                    <div className="overlay">
+                      <div className="text">View Item</div>
+                    </div>
+                  </div>
+                  <div className="productName">Nora Flemming Mini - Owl</div>
+                  <div className="price">$99.99</div>
+                </div>
+              </a>
+              <a href="/products/item" alt="item">
+                <div className="item">
+                  <div className="container">
+                    <img src={owl} alt="itemImg"></img>
+                    <div className="overlay">
+                      <div className="text">View Item</div>
+                    </div>
+                  </div>
+                  <div className="productName">Nora Flemming Mini - Owl</div>
+                  <div className="price">$99.99</div>
+                </div>
+              </a>
+            </div>
+            <div className="productRow">
+              <a href="/products/item" alt="item">
+                <div className="item">
+                  <div className="container">
+                    <img src={owl} alt="itemImg"></img>
+                    <div className="overlay">
+                      <div className="text">View Item</div>
+                    </div>
+                  </div>
+                  <div className="productName">Nora Flemming Mini - Owl</div>
+                  <div className="price">$99.99</div>
+                </div>
+              </a>
+              <a href="/products/item" alt="item">
+                <div className="item">
+                  <div className="container">
+                    <img src={owl} alt="itemImg"></img>
+                    <div className="overlay">
+                      <div className="text">View Item</div>
+                    </div>
+                  </div>
+                  <div className="productName">Nora Flemming Mini - Owl</div>
+                  <div className="price">$99.99</div>
+                </div>
+              </a>
+              <a href="/products/item" alt="item">
+                <div className="item">
+                  <div className="container">
+                    <img src={owl} alt="itemImg"></img>
+                    <div className="overlay">
+                      <div className="text">View Item</div>
+                    </div>
+                  </div>
+                  <div className="productName">Nora Flemming Mini - Owl</div>
+                  <div className="price">$99.99</div>
+                </div>
+              </a>
+              <a href="/products/item" alt="item">
+                <div className="item">
+                  <div className="container">
+                    <img src={owl} alt="itemImg"></img>
+                    <div className="overlay">
+                      <div className="text">View Item</div>
+                    </div>
+                  </div>
+                  <div className="productName">Nora Flemming Mini - Owl</div>
+                  <div className="price">$99.99</div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Home;
